8aef8b7c6fd188c72c4750965b9b2a85f8a56c42
juned.khan <juned3you@gmail.com>
2017-04-26 16:46:01 +0530

    Modified last week files

--- src/main/java/com/june/easycover/utils/User.java
+++ src/main/java/com/june/easycover/utils/User.java
@@ -4,6 +4,7 @@ public class User {
 
     private String firstName;
     private String lastName;
+    private Integer age;
 
     public String getFirstName() {
         return firstName;
@@ -19,6 +20,14 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
 
