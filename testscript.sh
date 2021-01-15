96f9a102c94712f8aae2474dfaf956d1b9f223d1
Mohammad Juned Khan <MohammadJuned.Khan@optiva.com>
2021-01-15 17:05:32 +0530

    TestScript

---
+++ testscript.sh
@@ -0,0 +1,1 @@
+sudo apt-get update -y && sudo apt install apache2 -y && sudo service apache2 restart && cd /var/www/html && sudo chmod 777 index.html && echo '<html><h1>Welcome To My Webpage MAC1</h1></html>' > index.html
