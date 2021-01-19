9aef8db485864bc88c73c7c64dd4020b9c61a33a
Mohammad Juned Khan <MohammadJuned.Khan@optiva.com>
2021-01-19 21:24:06 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ sudo apt install apache2 -y
 sudo service apache2 restart 
 cd /var/www/html 
 sudo chmod 777 index.html 
-echo "<html><h1>Welcome To My Webpage MAC1</h1><br><img src='https://$1.azureedge.net/$2/1.jpg'/> <br><img src='https://$1.azureedge.net/$2/2.jpg'/> </html>" > index.html
+echo "<html><h1>Welcome To My Webpage</h1><br><img src='https://$1.azureedge.net/$2/1.jpg'/> <br><img src='https://$1.azureedge.net/$2/2.jpg'/> </html>" > index.html
